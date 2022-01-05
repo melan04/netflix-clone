dff98c0c49b0a3fbe7abfe25db312b47ad7f47be
Mélany Condé <melany.conde@gmail.com>
2022-01-05 17:26:07 +0100

    Réalisation de la navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {useState} from "react";
+import './Nav.scss';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+function Nav() {
+
+    const [navBlack, setNavBlack] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const transitionNav = () => {
+        window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false);
+    };
+    useState(()=> {
+        document.addEventListener("scroll", transitionNav);
+    });
+
+    const handleClick = () => {
+        console.log(toggleMenu);
+        toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
+    };
+    
+    /* scroll */
+/* scroll */
+
+    return (
+        <div className={`nav ${
+            navBlack || toggleMenu ? "nav--black" : " nav--transparent"} ${toggleMenu && "show"}`}>
+            <button className='nav__burger' onClick={handleClick} >
+                <MenuIcon/>
+                </button>
+            <img src='./images/logo.png' className="nav__logo" alt="Netflix" />
+            <nav className='nav__links'>
+                <a href='/' className='nav__link'>
+                    Accueil
+                </a>
+                <a href='/' className='nav__link'>
+                    Séries
+                </a>
+                <a href='/' className='nav__link'>
+                    Films
+                </a>
+            </nav>
+            <div className="nav__actions">
+                <a href="/" className="nav__action">
+                    <SearchIcon/>
+                </a>
+                <a href="/" className="nav__action">
+                    DIRECT
+                </a>
+                <a href="/" className="nav__action">
+                    <CardGiftcardIcon/>
+                </a>
+                <a href="/" className="nav__action">
+                    <NotificationsIcon/>
+                </a>
+                
+                <a href="/" className="nav__action">
+                    <img src='./images/avatar.png' className="nav__avatar" alt='avatar par défaut' />
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
